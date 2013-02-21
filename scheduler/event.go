66b92404b70a95f2365baba8f388590a0a1886ad
Nick Cox <nickcox1008@gmail.com>
2013-02-21 12:04:56 -0700

    better use of the time package; adding 1 day instead of 24 hours; if it has been looking for an entire year then there is not an enabled day, we should bail

--- scheduler/event.go
+++ scheduler/event.go
@@ -19,28 +19,34 @@ type Event struct {
 func (e *Event) UpdateNextTime() error {
 	// e.nextTime is thisTime right now
 	now := time.Now()
-	today := e.NextTime.Weekday()
-	_, thisWeek := e.NextTime.ISOWeek()
+	day := int(e.NextTime.Weekday()) + 1 // weekday = 0-6, start tomorrow (+1)
+	_, week := e.NextTime.ISOWeek()      // year, week(1-53)
+	week -= 1                            // convert to 0-52
+	nextYear := now.AddDate(1, 0, 0)     // Next year for detecting empty events
 
-	firstTime := true
 	for {
-		day := 0
-		if firstTime {
-			day = int(today) + 1
-			firstTime = false
-		}
 		for ; day < 7; day++ {
 			// Add one more day to the wait time
-			e.NextTime = e.NextTime.Add(24 * time.Hour)
-			// If we're up to today and it is enabled then we're done
-			if e.NextTime.After(now) && e.RepeatWeeks[(thisWeek-1)%52] && e.RepeatDays[day] {
+			e.NextTime = e.NextTime.AddDate(0, 0, 1)
+			// We're done if nextTime is after today and today is enabled
+			if e.NextTime.After(now) &&
+				e.RepeatWeeks[week] &&
+				e.RepeatDays[day] {
 				return nil
 			}
 		}
-		thisWeek++
+		// Check to make sure we havn't been looping forever
+		if e.NextTime.After(nextYear) {
+			break
+		}
+		week++
+		if week > 52 {
+			week -= 53
+		}
+		day = 0
 	}
 
-	return errors.New("This point sould never be reached")
+	return errors.New("There were no enabled days for this event")
 }
 
 func (e *Event) Update(
